dbd760ac66c77a64f7a985fdeebd7acff2cd08f0
Jackson Shen <shenj7@rose-hulman.edu>
2023-01-18 17:34:11 -0500

    fixing

--- implementation/assembler/translate_file_entry.py
+++ implementation/assembler/translate_file_entry.py
@@ -8,7 +8,6 @@ def command_line_parser(main_args):
     parser = ArgumentParser(description="translate violet team instructions to machine language. does not check whether an immediate is small enough, or if the register is within range")
     parser.add_argument('-f',
                         '--file',
-                        nargs="+",
                         help="file to translate")
     args = parser.parse_args(main_args)
 
@@ -18,12 +17,13 @@ def command_line_parser(main_args):
 def get_labels(file, starting_add):
     labels = dict()
     address = starting_add
-    for instruction in f:
+    file.seek(0)
+    for instruction in file:
         loc = instruction.find(":")
         if loc != -1:
             label = instruction[0:loc]
-            labels[label] = loc
-        loc = loc + 2
+            labels[label] = address
+        address = address + 2
 
     return labels
 
@@ -32,7 +32,7 @@ def replace_label(new_inst, labels):
     replaced = ""
     for label in labels:
         if label in new_inst:
-            replaced = new_inst.replace(label, labels[label])
+            replaced = new_inst.replace(label, str(labels[label]))
         else:
             replaced = new_inst
 
@@ -40,7 +40,8 @@ def replace_label(new_inst, labels):
 
 
 def make_machine_file(file, labels):
-    f = open(file.name + "_machine", "x")
+    f = open(file.name + "_machine", "w")
+    file.seek(0)
     for instruction in file:
         loc = instruction.find(":")
         new_inst = ""
@@ -48,7 +49,10 @@ def make_machine_file(file, labels):
             new_inst = instruction[loc+1:]
         else:
             new_inst = instruction
+        print(new_inst)
+        print(labels)
         new_inst = replace_label(new_inst, labels)
+        new_inst = typeswitch(parse_inst(new_inst))
         f.write(new_inst)
 
     return f
@@ -59,7 +63,7 @@ def main(main_args=""):
     args = command_line_parser(main_args)
     file = args.file
     f = open(file, "r")
-    labels = get_labels(f)
+    labels = get_labels(f, 0)
     newfile = make_machine_file(f, labels)
     print(f"machine translation for {file} written to {newfile.name}")
 
