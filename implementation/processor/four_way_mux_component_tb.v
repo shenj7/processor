0fb178374144a5fc7e95eceaffea946f9279bd23
Jackson Shen <shenj7@rose-hulman.edu>
2023-02-13 09:07:36 -0500

    error fixing

--- implementation/processor/four_way_mux_component_tb.v
+++ implementation/processor/four_way_mux_component_tb.v
@@ -6,14 +6,14 @@ module four_way_mux_component_tb();
 //Inputs
 reg rst;
 reg clk;
-wire signed [1:0] OP;
-wire signed [15:0] IN0;
-wire signed [15:0] IN1;
-wire signed [15:0] IN2;
-wire signed [15:0] IN3;
+reg [1:0] OP;
+reg [15:0] IN0;
+reg [15:0] IN1;
+reg [15:0] IN2;
+reg [15:0] IN3;
 
 //Outputs
-wire signed [15:0] out;
+wire [15:0] out;
 
 parameter HALF_PERIOD = 50;
 integer cycle_counter = 0;
@@ -31,61 +31,60 @@ end
 
 four_way_mux_component UUT (
     .clock(clk),
-	 .in0(IN0),
-	 .in1(IN1),
-	 .in2(IN2),
-	 .in3(IN3),
-	 .op(OP),
-	 .reset(rst),
-	 .out(out)
+    .in0(IN0),
+    .in1(IN1),
+    .in2(IN2),
+    .in3(IN3),
+    .op(OP),
+    .reset(rst),
+    .out(out)
 );
 
 
 initial begin
-    dir = 0;
     rst = 1;
     #(100*HALF_PERIOD);
     rst = 0;
-	 //-----TEST 1-----
+    //-----TEST 1-----
     OP = 00;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 IN2 = 0000000000000010;
-	 IN3 = 0000000000000011;
-	 if (out != 0000000000000000) begin
-            failures = failures + 1;
-            $display("Test 1 Failed output = %d, expecting = %d", out, 0000000000000000);
-        end
-	 //-----TEST 2-----
+    IN0 = 0000000000000000;
+    IN1 = 0000000000000001;
+    IN2 = 0000000000000010;
+    IN3 = 0000000000000011;
+    if (out != 0000000000000000) begin
+        failures = failures + 1;
+        $display("Test 1 Failed output = %d, expecting = %d", out, 0000000000000000);
+    end
+    //-----TEST 2-----
     OP = 01;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 IN2 = 0000000000000010;
-	 IN3 = 0000000000000011;
-	 if (out != 0000000000000001) begin
-            failures = failures + 1;
-            $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000001);
-        end
-	 //-----TEST 3-----
+    IN0 = 0000000000000000;
+    IN1 = 0000000000000001;
+    IN2 = 0000000000000010;
+    IN3 = 0000000000000011;
+    if (out != 0000000000000001) begin
+        failures = failures + 1;
+        $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000001);
+    end
+    //-----TEST 3-----
     OP = 10;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 IN2 = 0000000000000010;
-	 IN3 = 0000000000000011;
-	 if (out != 0000000000000010) begin
-            failures = failures + 1;
-            $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000010);
-        end
-	 //-----TEST 4-----
+    IN0 = 0000000000000000;
+    IN1 = 0000000000000001;
+    IN2 = 0000000000000010;
+    IN3 = 0000000000000011;
+    if (out != 0000000000000010) begin
+        failures = failures + 1;
+        $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000010);
+    end
+    //-----TEST 4-----
     OP = 11;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 IN2 = 0000000000000010;
-	 IN3 = 0000000000000011;
-	 if (out != 0000000000000011) begin
-            failures = failures + 1;
-            $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000011);
-        end
+    IN0 = 0000000000000000;
+    IN1 = 0000000000000001;
+    IN2 = 0000000000000010;
+    IN3 = 0000000000000011;
+    if (out != 0000000000000011) begin
+        failures = failures + 1;
+        $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000011);
+    end
     $display("TESTS COMPLETE. \n Failures = %d", failures);
     $stop;
 end
--- implementation/processor/two_way_mux_tb.v
+++ implementation/processor/two_way_mux_tb.v
@@ -6,9 +6,9 @@ module two_way_mux_tb();
 //Inputs
 reg clk;
 reg reset;
+reg [15:0] in0;
 reg [15:0] in1;
-reg [15:0] in2;
-reg [0] op;
+reg op;
 
 //Outputs
 wire [15:0] out;
@@ -29,34 +29,33 @@ end
 two_way_mux UUT (
     .clk(clk),
     .reset(reset),
+    .in0(in0),
     .in1(in1),
-    .in2(in2),
     .op(op),
     .out(out)
 );
 
 
 initial begin
-    dir = 0;
-    rst = 1;
+    reset = 1;
     #(100*HALF_PERIOD);
-    rst = 0;
-	 //-----TEST 1-----
-    OP = 0;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 if (out != 0000000000000000) begin
-            failures = failures + 1;
-            $display("Test 1 Failed output = %d, expecting = %d", out, 0000000000000000);
-        end
-	 //------TEST 2-----
-    OP = 1;
-	 IN0 = 0000000000000000;
-	 IN1 = 0000000000000001;
-	 if (out != 0000000000000001) begin
-            failures = failures + 1;
-            $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000001);
-        end
+    reset = 0;
+    //-----TEST 1-----
+    op = 0;
+    in0 = 0000000000000000;
+    in1 = 0000000000000001;
+    if (out != 0000000000000000) begin
+        failures = failures + 1;
+        $display("Test 1 Failed output = %d, expecting = %d", out, 0000000000000000);
+    end
+    //------TEST 2-----
+    op = 1;
+    in0 = 0000000000000000;
+    in1 = 0000000000000001;
+    if (out != 0000000000000001) begin
+        failures = failures + 1;
+        $display("Test 2 Failed output = %d, expecting = %d", out, 0000000000000001);
+    end
     $display("TESTS COMPLETE. \n Failures = %d", failures);
     $stop;
 end
