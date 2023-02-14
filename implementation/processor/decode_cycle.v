a6ad722d5fd82d88d4dc7d50f6849309347245ff
Jackson Shen <shenj7@rose-hulman.edu>
2023-02-14 13:58:59 -0500

    decode and fetch

--- implementation/processor/decode_cycle.v
+++ implementation/processor/decode_cycle.v
@@ -1,33 +1,57 @@
 module decode_cycle();
 
-input [15:0] inst;
+input [15:0] ir;
+input [15:0] pc;
 input clk;
-input wrt;
-input rst;
+input [15:0] writedata;
+input regwrite;
 
 output [15:0] inst;
+output [15:0] pcout;
+output [15:0] a;
+output [15:0] b;
+output [3:0] rdout;
+output [15:0] imm;
+
+
+wire [3:0] rs1;
+wire [3:0] rs2;
+wire [3:0] rd;
+
 
 ir_component ir (
     .clock(clk),
-    .in1(),
+    .in1(ir),
     .reset(),
-    .rs1(),
-    .rs2(),
-    .rd()
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(rd)
 );
 
-alu_component small_alu (
-    .inst_id(),
-    .in0(),
-    .in1(),
-    .reset(rst),
-    .out(),
-    .zero(),
-    .pos()
+reg_file_component rf (
+    .clock(clk),
+    .rs1(rs1),
+    .rs2(rs2),
+    .rd(rd),
+    .writedata(writedata),
+    .write(regwrite),
+    .reset(),
+    .reg1(a),
+    .reg2(b)
+);
+
+imm_gen_component ig (
+    .clock(clk),
+    .inst(ir),
+    .reset(),
+    .out(imm)
 );
 
 always @(posedge clock)
 begin
+    pcout = pc;
+    rdout = rd;
+    inst = ir;
 
 
 end
--- implementation/processor/fetch_cycle.v
+++ implementation/processor/fetch_cycle.v
@@ -1,28 +1,41 @@
 module fetch_cycle();
 
-input [15:0] addr;
+input [15:0] pc;
+input pcwrite;
 input clk;
-input wrt;
 input rst;
 
-output [15:0] inst;
+output [15:0] ir;
+output [15:0] currpc;
+
+wire [15:0] ir;
+wire [15:0] newpc;
 
 inst_mem_component im (
+    .addr(pc),
+    .clk(clk),
+    .out(ir)
 );
 
 alu_component small_alu (
-    .inst_id(),
-    .in0(),
-    .in1(),
+    .inst_id(1'b0),
+    .in0(pc),
+    .in1(2'b01),
     .reset(rst),
-    .out(),
-    .zero(),
+    .out(newpc),
+    .zero(), // should be empty
     .pos()
 );
 
 always @(posedge clock)
 begin
-
+    if (rst == 0) begin
+        //rst stuff
+    end
+    currpc = pc;
+    if (pcwrite == 1) begin
+        pc = newpc;
+    end
 
 end
 
